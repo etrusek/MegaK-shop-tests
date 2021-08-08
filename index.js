565fecd66e1872d8077c7ccb8be09aaab0d869da
Mariusz Lewandowski <mar.lewandowski@gmail.com>
2021-08-08 11:47:12 +0200

    w5d2tast

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-class Counting {
-    constructor() {
-        this.equation = {
-            firstNumber: null,
-            secondNumber: null,
-            sign: null,
+class Calculator {
+    constructor(firstNumber, secondNumber) {
+        const a = Number(firstNumber);
+        const b = Number(secondNumber);
+        console.log(firstNumber, a)
+        if (Number.isNaN(a) || firstNumber === '') {
+            throw new Error('nieprawidłowo podana liczba pierwsza')
         }
-    };
-
-    addToFirstNumber(num) {
-        this.equation.firstNumber = this.equation.firstNumber === null ? Number(num) : String(this.equation.firstNumber) + String(num);
-    };
-
-    addSign(sign) {
-        this.equation.sign = sign;
-    };
-
-    doMath() {
-        switch (this.equation.sign) {
-            case "+":
-                return this.equation.secondNumber + this.equation.firstNumber;
-                break;
-            case "-":
-                return this.equation.secondNumber - this.equation.firstNumber;
-                break;
-            case "*":
-                return this.equation.secondNumber * this.equation.firstNumber;
-                break;
-            case "/":
-                return this.equation.secondNumber / this.equation.firstNumber;
-                break;
-
+        if (Number.isNaN(b) || secondNumber === '') {
+            throw new Error('nieprawidłowo podana liczba druga')
         }
+        this.a = a;
+        this.b = b;
+
     }
+
+    add() {
+        return this.a + this.b;
+    };
+
+    subtract() {
+        return this.a - this.b;
+    };
+
+    multiply() {
+        return this.a * this.b;
+    };
+
+    divide() {
+        if (this.b === 0){throw new Error("nie wolno dzielić przez 0")}
+        return this.a / this.b;
+    };
 }
--- ui.js
+++ ui.js
@@ -1,66 +1,26 @@
-const ulCalc = document.querySelector('.ulCalc');
-const ulActions = document.querySelector('.ulActions');
-const calcOutput = document.querySelector('.output');
+const form = document.querySelector('form');
+const output = document.querySelector('article');
 
-const arrOfNumb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const actions = ['+', "-", "*", '/', '='];
-
-const calc = new Counting;
-
-//obsługa
-
-function numberEvent(e) {
-    const number = e.target.dataset.num;
-    if (calc.equation.secondNumber === null) {
-        calc.addToFirstNumber(number);
-        console.log(calc.equation.firstNumber, calc.equation.secondNumber);
-        calcOutput.style.color = 'black';
-        calcOutput.textContent = calc.equation.firstNumber;
-    } else {
-        // mamy drugą liczbę teraz usupełniamy pierwszą i obliczamy.
-        calc.addToFirstNumber(number);
-        console.log(calc.equation.secondNumber, calc.equation.sign, calc.equation.firstNumber);
-        calcOutput.textContent = `${calc.equation.secondNumber} ${calc.equation.sign} ${calc.equation.firstNumber} = ${calc.doMath()}`;
+const doMath = (e) => {
+    e.preventDefault();
+    const firstNumber = e.target[0].value;
+    const secondNumber = e.target[1].value;
+    try {
+        const calc = new Calculator(firstNumber, secondNumber);
+        output.innerText = `Wynik dodawania \n
+    ${firstNumber} + ${secondNumber} = ${calc.add(firstNumber, secondNumber)} \n
+    Wynik odejmowania \n
+    ${firstNumber} + ${secondNumber} = ${calc.subtract(firstNumber, secondNumber)} \n
+    Wynik mnożenia \n
+    ${firstNumber} * ${secondNumber} = ${calc.multiply(firstNumber, secondNumber)} \n
+    Wynik dzielenia \n
+    ${firstNumber} / ${secondNumber} = ${calc.divide(firstNumber, secondNumber)} \n
+    `
+    } catch (e) {
+        console.log("nie działa", e)
     }
+
 }
 
-function actionEvent(e) {
-    const action = e.target.dataset.action;
-    if (calc.equation.firstNumber === null) {
-        // We're clicking sign, but first we have to give a number.
-        calcOutput.textContent = `Podaj liczbę...`;
-        calcOutput.style.color = 'red';
-    } else if (calc.equation.secondNumber !== null){
-        console.log('a')
-        calc.equation.secondNumber = calc.doMath();
-        calc.addSign(action);
-        calc.equation.firstNumber = null;
-    }    else {
-        // Mamy jedną liczbę i znak (teraz). Zapisujemy pierwszą liczbę jako drugą i zapisujemy znak.
-        calc.equation.secondNumber = calc.equation.firstNumber;
-        calc.equation.firstNumber = null;
-        calc.addSign(action);
-    }
-}
 
-// wydrukowanie przycisków
-
-arrOfNumb.forEach(btnArr => {
-    const btn = document.createElement('button');
-    btn.classList.add(`btn${btnArr}`, 'calcBtn');
-    btn.setAttribute('data-num', btnArr);
-    btn.textContent = btnArr;
-    btn.addEventListener('click', numberEvent)
-    ulCalc.appendChild(btn);
-})
-
-//wydrukowanie znaków = - * / =
-
-actions.forEach(btnAct => {
-    const btn = document.createElement('button');
-    btn.classList.add(`btn${btnAct}`, 'calcBtn', 'calcBtnAct');
-    btn.setAttribute('data-action', btnAct);
-    btn.textContent = btnAct;
-    btn.addEventListener('click', actionEvent)
-    ulActions.appendChild(btn);
-})
+form.addEventListener('submit', doMath)
