3f26fd58b9df762110969f8bcec96113c9f674a2
Mariusz Lewandowski <mar.lewandowski@gmail.com>
2021-08-24 17:39:18 +0200

    s2w2d2t1

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-// task2
-const {readdir, stat} = require('fs').promises;
-const arrOfFiles = []
-const path = './Projekty';
+const {dirname, basename, extname, resolve, relative} = require('path');
+const {stat} = require('fs').promises;
 
-const listingFiles = async (path, tree) => {
-    const files = await readdir(path);
-    // console.log(files)
-    for (const file of files) {
-        const isFolder = (await stat(`${path}/${file}`)).isDirectory();
-        console.log(tree, file, ' - ', isFolder ? "Folder" : "Plik");
-        if (isFolder) {
-            await listingFiles(`${path}/${file}`, `${tree}--`)
+const task1 = async (par) => {
+    const absolutePath = relative(__dirname, par);
+    const isHaker = absolutePath[0] === '.';
+    try {
+        const isFile = (await stat(absolutePath)).isFile();
+        const isFolder = (await stat(absolutePath)).isDirectory();
+        if (isHaker) {
+            console.log('Podano ', par, " Mnie się nie hakeruje, hakerczyku", "ODMOWA DOSTĘPU!!! BLOKADA NA GRY");
+            console.log(' ');
+        } else {
+            console.log('Ścieżka z folderu głównego:', `./${absolutePath}`);
+            console.log('Pełna ścieżka to:     ', resolve(__dirname, absolutePath));
+            console.log('Katalog nadrzędny to: ', dirname(resolve(__dirname, absolutePath)));
+
+            if (isFile) {
+                console.log('Wskazana lokalizacja prowadzi do pliku');
+                console.log('Nazwa pliku to:       ', basename(absolutePath));
+                console.log('Rozszerzenie to:      ', extname(absolutePath));
+            } else if (isFolder) {
+                console.log('Wskazana lokalizacja prowadzi do folderu');
+                console.log('Nazwa folderu to:     ', basename(absolutePath));
+                console.log('Folder nie posiada rozszerzenia.');
+            }
         }
+    } catch (e) {
+        const isFile = false;
+        const isFolder = false;
+        console.log('Ścieżka z folderu głównego:', `.\\${absolutePath}`);
+        console.log('Pełna ścieżka to:     ', resolve(__dirname, absolutePath));
+        console.log('Katalog nadrzędny to: ', dirname(resolve(__dirname, absolutePath)));
+        console.log('Nie istnieje wskazana lokalizacja');
+        console.log('Nazwa pliku/folderu to: ', basename(absolutePath));
+        console.log('Rozszerzenie nieistniejącego pliku to: ', extname(absolutePath));
     }
+
+
+    console.log(' ')
 }
-listingFiles(path, '').then(a => arrOfFiles.push(a));
-console.log(arrOfFiles, 'koniec');
 
-
-//task1
-
-// const {writeFile, access, readFile, readdir, mkdir, stat} = require('fs').promises;
-// const {constants} = require('fs');
-// const FILE_CONTENT = [1, 2, 7, 20, 56, 22];
-//
-// (async () => {
-//     try {
-//         await access('./data/input1.json', constants.F_OK)
-//     } catch (e) {
-//         await writeFile('./data/input1.json', JSON.stringify(FILE_CONTENT), {flag: 'a'});  // tworzy plik i zapisuje w nim tablicę
-//     }
-//     const fileContent = JSON.parse( await readFile('./data/input1.json', 'utf8'));
-//     let counter = 0
-//     fileContent.forEach(num => counter += num);
-//     const sumToSave = counter.toString();
-//     await writeFile('./data/sum.txt', sumToSave);
-//     console.log(counter);
-// })()
-
-
-
-
+// task1(process.argv[2]);
+// task1('C:\\Users\\Profesjonalista\\Desktop\\nauka-programowania\\MegaK');
+// task1('./data');
+// task1('C:\\Users\\Profesjonalista\\Desktop\\nauka-programowania\\MegaK\\week4\\MegaK-shop-tests\\index.js');
+// task1('C:\\Users\\Profesjonalista\\Desktop\\nauka-programowania\\MegaK\\week4\\MegaK-shop-tests\\data\\sum.txt');
+task1('C:\\Users\\Profesjonalista\\Desktop\\nauka-programowania\\MegaK\\week4\\MegaK-shop-tests\\data\\suma.txt');
+// task1('../../aaa');
